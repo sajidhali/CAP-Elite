e883b0820a09f92a90de4de17c7e2a91e41704b8
sajidhali <85603440+sajidhali@users.noreply.github.com>
2023-02-07 19:30:31 +0530

    Create Java HashSet.java

---
+++ EasyCollections/Java HashSet.java
@@ -0,0 +1,11 @@
+       
+     
+     HashSet<String> n = new HashSet<>();  
+     for(int i=0;i< t;i++) {  
+        String str="";
+        str+=pair_left[i]+" "+pair_right[i];
+        n.add(str);
+        System.out.println(n.size());  
+    }
+        
+     
