5c87187fc796f14a9ad5e9ef272b5df2249e28c5
sajidhali <85603440+sajidhali@users.noreply.github.com>
2023-02-07 10:02:57 +0530

    Create Breaking the Records.java

---
+++ MonthPrep/EasyCollections/Breaking the Records.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+    // Write your code here
+        int sum = scores.get(0); int count = scores.get(0);
+        int high_breaker=0;
+        int low_breaker=0;
+        ArrayList<Integer> gg=new ArrayList<>();
+        for(int i=0;i<scores.size();i++)
+        {
+            if(scores.get(i)<sum){
+                sum=scores.get(i);
+                low_breaker++;
+            }
+            else if(scores.get(i)>count){
+                count=scores.get(i);
+                high_breaker++;
+            }
+            }
+       gg.add(high_breaker);
+        gg.add(low_breaker);
+    return gg;
+    }    
+        
+
+    }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] scoresTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> scores = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresTemp[i]);
+            scores.add(scoresItem);
+        }
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
