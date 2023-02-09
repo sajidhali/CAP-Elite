d90d6cd41358b5750d38808e71a4096a1e43a00a
sajidhali <85603440+sajidhali@users.noreply.github.com>
2023-02-09 20:10:19 +0530

    Create Dynamic Array.java

---
+++ MonthPrep/Week 2/Dynamic Array.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+    // Write your code here
+    int l = 0;
+    List<Integer> a = new ArrayList<Integer>();
+    List<List<Integer>> arr = new ArrayList<List<Integer>>(n);
+    for (int i = 0; i < n; i++) {
+        arr.add(new ArrayList<Integer>());
+    }
+
+    for (List<Integer> query : queries) {
+        int q = query.get(0);
+        int x = query.get(1);
+        int y = query.get(2);
+        int j;
+        if (q == 1) {
+            j = ((x ^ l) % n);
+            arr.get(j).add(y);
+        } else {
+            j = ((x ^ l) % n);
+            int size = arr.get(j).size();
+            l = arr.get(j).get(y % size);
+            a.add(l);
+        }
+    }
+
+    return a;
+}
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        for (int i = 0; i < q; i++) {
+            String[] queriesRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> queriesRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowTempItems[j]);
+                queriesRowItems.add(queriesItem);
+            }
+
+            queries.add(queriesRowItems);
+        }
+
+        List<Integer> result = Result.dynamicArray(n, queries);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
