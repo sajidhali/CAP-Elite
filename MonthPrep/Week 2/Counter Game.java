8277c4aba77c7243f5bbfa53357f87e882d52ada
sajidhali <85603440+sajidhali@users.noreply.github.com>
2023-02-13 20:45:22 +0530

    Create Counter Game.java

---
+++ MonthPrep/Week 2/Counter Game.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'counterGame' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static String counterGame(long n) {
+    // Write your code here
+        String bString = Long.toBinaryString(n);
+        int bSum = Stream.of(bString.split(""))
+                .mapToInt(Integer::parseInt)
+                .sum();
+        int attempt = bSum + bString.length() - bString.lastIndexOf("1");
+        return attempt % 2 != 0 ? "Louise" : "Richard";
+    }
+    }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                String result = Result.counterGame(n);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
